bc68af815bbb589becb31599f6d2b90717406e77
sara-alaa634 <zaa3748@gmail.com>
2025-11-09 23:37:11 +0200

    06 Generic Repository & Unit Of Work

---
+++ Ecommerce.Domain/Contracts/IGenaricReposatory.cs
@@ -0,0 +1,19 @@
+﻿using Ecommerce.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Domain.Contracts
+{
+    public interface IGenaricReposatory<TEntity,TKey> where TEntity : BaseEntity<TKey> 
+    {
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<TEntity?> GetByIdAsync(TKey id);
+        Task AddAsync(TEntity entity);
+        void Update(TEntity entity);
+        void Remove(TEntity entity);
+
+    }
+}
---
+++ Ecommerce.Prisastance/Reposatories/GenaricReposatory.cs
@@ -0,0 +1,34 @@
+﻿using Ecommerce.Domain.Contracts;
+using Ecommerce.Domain.Entities;
+using Ecommerce.Prisastance.Data.DbContexts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Prisastance.Reposatories
+{
+    public class GenaricReposatory<TEntity, TKey> : IGenaricReposatory<TEntity, TKey> where TEntity : BaseEntity<TKey>
+    {
+        private readonly StoreDbContext _dbContext;
+
+        public GenaricReposatory(StoreDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task AddAsync(TEntity entity)=> await _dbContext.Set<TEntity>().AddAsync(entity);
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        => await _dbContext.Set<TEntity>().ToListAsync();
+
+        public async Task<TEntity?> GetByIdAsync(TKey id)
+        => await _dbContext.Set<TEntity>().FindAsync(id);
+
+        public void Remove(TEntity entity)=> _dbContext.Set<TEntity>().Remove(entity);
+
+        public void Update(TEntity entity)
+        => _dbContext.Set<TEntity>().Update(entity);
+    }
+}
