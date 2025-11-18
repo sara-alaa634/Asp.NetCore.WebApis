61ef2d921a23933398faad2140fe39f4c2be733c
sara-alaa634 <zaa3748@gmail.com>
2025-11-18 20:05:04 +0200

    03 Caching Repository

---
+++ ECommerce.Web/Attributes/RadisCacheAttribute.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ECommerce.Web.Attributes
+{
+    public class RadisCacheAttribute : Microsoft.AspNetCore.Mvc.Filters.ActionFilterAttribute
+    {
+      
+        // Async Excuted After / Before Endpoint
+        public override Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            return base.OnActionExecutionAsync(context, next);
+        }
+    }
+}
---
+++ Ecommerce.Prisastance/Reposatories/CacheReposatory.cs
@@ -0,0 +1,32 @@
+﻿using Ecommerce.Domain.Contracts;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Prisastance.Reposatories
+{
+    internal class CacheReposatory : ICacheReposatory
+    {
+        private readonly IDatabase _database;
+
+        // Connection To Radis
+        public CacheReposatory(IConnectionMultiplexer connection)
+        {
+            _database = connection.GetDatabase();
+        }
+        public async Task<string?> GetAsync(string CacheKey)
+        {
+           var CahceValue =await  _database.StringGetAsync(CacheKey);
+           if(CahceValue.IsNullOrEmpty) return null;
+           return CahceValue.ToString();
+        }
+
+        public async Task SetAsync(string CacheKey, string CacheValue, TimeSpan TimeToLive)
+        {
+            await _database.StringSetAsync(CacheKey, CacheValue, TimeToLive);
+        }
+    }
+}
