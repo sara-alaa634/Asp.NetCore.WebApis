d78b447d20b8815d6e3701858e4f345d28b9b2a3
sara-alaa634 <zaa3748@gmail.com>
2025-11-23 20:33:22 +0200

    02 Result Pattern Implementation

---
+++ Ecommerce.Shared/CommanResult/ErrorType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Shared.CommanResult
+{
+    public enum ErrorType
+    {
+        Failure=0,
+        Validation=1,
+        NotFound=2,
+        Unauthorized=3,
+        Forbidden =4,
+        invalidCredentials=5,
+    }
+}
